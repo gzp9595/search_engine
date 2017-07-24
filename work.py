ef9a361c94e8a41c00883061d52f660b075c74c0
zhonghaoxi <zhonghaoxi@yeah.net>
2017-07-25 00:17:08 +0800

    gg

--- work.py
+++ work.py
@@ -238,7 +238,7 @@ def search_new():
         args["index"] = ""
     if not("doc_type" in request.args):
         args["doc_type"] = ""
-    return render_template("search_new.html", args=request.args, result=result, query=json.dumps(request.args))
+    return render_template("search_new.html", args=request.args, result=result, query=request.args)
 
 
 @app.route('/adddata',methods=["POST","GET"])
