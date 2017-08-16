1ff5be9e7ed5f3e4e6c27724869336dcd634cff9
zhonghaoxi <zhonghaoxi@yeah.net>
2017-08-16 15:03:07 +0800

    gg

--- tools/init.py
+++ tools/init.py
@@ -2,44 +2,13 @@ import os
 
 os.system("curl -XPUT http://localhost:9200/law")
 print
-os.system("""curl -XPOST http://localhost:9200/law/small_data/_mapping -d'
-{
-    "small_data": {
-        "_all": {
-            "analyzer": "ik_max_word",
-            "search_analyzer": "ik_smart",
-            "term_vector": "no",
-            "store": "false"
-        },  
-        "properties": {
-            "content": {
-                "type": "text",
-                "analyzer": "ik_max_word",
-                "search_analyzer": "ik_smart",
-                "include_in_all": "true",
-                "boost": 8
-            },
-            "Title": {
-                "type": "text",
-                "analyzer": "ik_max_word",
-                "search_analyzer": "ik_smart",
-                "include_in_all": "true",
-                "boost": 8
-            },
-            "FLYJ": {
-                "type" : "nested"
-            }
-        }
-    }
-}'""")
-print
 
 print
 os.system("""curl -XPOST http://localhost:9200/law/big_data/_mapping -d'
 {
     "big_data": {
         "_all": {
-            "analyzer": "ik_max_word",
+            "analyzer": "ik_smart",
             "search_analyzer": "ik_smart",
             "term_vector": "no",
             "store": "false"
@@ -47,14 +16,14 @@ os.system("""curl -XPOST http://localhost:9200/law/big_data/_mapping -d'
         "properties": {
             "content": {
                 "type": "text",
-                "analyzer": "ik_max_word",
+                "analyzer": "ik_smart",
                 "search_analyzer": "ik_smart",
                 "include_in_all": "true",
                 "boost": 8
             },
             "Title": {
                 "type": "text",
-                "analyzer": "ik_max_word",
+                "analyzer": "ik_smart",
                 "search_analyzer": "ik_smart",
                 "include_in_all": "true",
                 "boost": 8
