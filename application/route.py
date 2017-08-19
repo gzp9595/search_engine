7a3ca1f3ad88e6d38d5e5b53cc2ed8b5d14334df
zhonghaoxi <zhonghaoxi@yeah.net>
2017-08-19 12:50:44 +0800

    gg

--- application/route.py
+++ application/route.py
@@ -49,7 +49,6 @@ def remove_all():
     return "Succeed delete " + str(elastic.remove_all(request.args["index"], request.args["doc_type"])["deleted"])
 
 
-@app.route('/search_new')
 @app.route('/search')
 def search():
     result = []
@@ -145,9 +144,9 @@ def search():
         print_time()
 
         for x in query_result["hits"]:
-            res = {"id" : x["_id"], "title" : x["_source"]["Title"],"shortcut": x["_source"]["AJJBQK"][:100]}
-            #res = {"id": x["_id"], "score": x["_source"]["score"]}
-            #for y in x["_source"]:
+            res = {"id": x["_id"], "title": x["_source"]["Title"], "shortcut": x["_source"]["AJJBQK"][:100]}
+            # res = {"id": x["_id"], "score": x["_source"]["score"]}
+            # for y in x["_source"]:
             #    res[y] = x["_source"][y]
             result.append(res)
 
@@ -160,7 +159,114 @@ def search():
         args["index"] = ""
     if not ("doc_type" in request.args):
         args["doc_type"] = ""
-    return json.dumps(result)#return render_template("search.html", args=request.args, result=result, query=request.args)
+    return json.dumps(
+        result)  # return render_template("search.html", args=request.args, result=result, query=request.args)
+
+
+@app.route('/search_new')
+def search_new():
+    result = []
+
+    if "doc_type" in request.args and "index" in request.args:
+        body = []
+
+        args = request.args
+
+        search_type = "content"
+        if "where_to_search" in args and args["search_content"] != "":
+            match_type = {
+                "0": "content",
+                "1": "WBSB",
+                "2": "AJJBQK",
+                "3": "CPYZ",
+                "4": "PJJG",
+                "5": "WBWB"
+            }
+            search_type = match_type[args["where_to_search"]]
+
+            body.append({"match": {search_type: args["search_content"]}})
+
+        if "name_of_case" in args and args["name_of_case"] != "":
+            body.append({"match": {"Title": args["name_of_case"]}})
+
+        if "case_number" in args and args["case_number"] != "":
+            body.append({"match": {"AJAH": args["case_number"]}})
+
+        if "name_of_court" in args and args["name_of_court"] != "":
+            body.append({"match": {"FYMC": args["name_of_court"]}})
+
+        if "level_of_court" in args and args["level_of_court"] != "0":
+            try:
+                value = int(args["level_of_court"])
+                body.append({"term": {"FYCJ": value}})
+            except ValueError:
+                pass
+
+        if "type_of_case" in args and args["type_of_case"] != "0":
+            try:
+                value = int(args["type_of_case"])
+                body.append({"term": {"AJLX": value}})
+            except ValueError:
+                pass
+
+        if "type_of_doc" in args and args["type_of_doc"] != "0":
+            try:
+                value = int(args["type_of_doc"])
+                body.append({"term": {"WSLX": value}})
+            except ValueError:
+                pass
+
+        if "caipan_from_year" in args and "caipan_from_month" in args and "caipan_from_day" in args and util.check_date(
+                args["caipan_from_year"], args["caipan_from_month"], args["caipan_from_day"]):
+            body.append({"range": {"CPRQ": {"gte": args["caipan_from_year"] + "-" + args[
+                "caipan_from_month"] + "-" + args["caipan_from_day"]}}})
+
+        if "caipan_to_year" in args and "caipan_to_month" in args and "caipan_to_day" in args and util.check_date(
+                args["caipan_to_year"], args["caipan_to_month"], args["caipan_to_day"]):
+            body.append({"range": {"CPRQ": {"lte": args["caipan_to_year"] + "-" + args[
+                "caipan_to_month"] + "-" + args["caipan_to_day"]}}})
+
+        if "judgement" in args and args["judgement"] != "":
+            body.append({"match": {"WBWB": args["judgement"]}})
+
+        if "fabu_from_year" in args and "fabu_from_month" in args and "fabu_from_day" in args and util.check_date(
+                args["fabu_from_year"], args["fabu_from_month"], args["fabu_from_day"]):
+            body.append({"range": {"PubDate": {"gte": args["fabu_from_year"] + "-" + args[
+                "fabu_from_month"] + "-" + args["fabu_from_day"]}}})
+
+        if "fabu_to_year" in args and "fabu_to_month" in args and "fabu_to_day" in args and util.check_date(
+                args["fabu_to_year"], args["fabu_to_month"], args["fabu_to_day"]):
+            body.append({"range": {"PubDate": {"lte": args["fabu_to_year"] + "-" + args[
+                "fabu_to_month"] + "-" + args["fabu_to_day"]}}})
+
+        if "name_of_law" in args and args["name_of_law"] != "":
+            new_body = [{"match": {"FLYJ.law_name": args["name_of_law"]}}]
+            if "num_of_tiao" in args and args["num_of_tiao"] != "":
+                new_body.append({"match": {"FLYJ.tiao_num": args["num_of_tiao"]}})
+            if "num_of_kuan" in args and args["num_of_kuan"] != "":
+                new_body.append({"match": {"FLYJ.kuan_num": args["num_of_kuan"]}})
+            body.append({"nested": {"path": "FLYJ", "query": {"bool": {"must": new_body}}}})
+
+        query_result = elastic.search_doc(request.args["index"], request.args["doc_type"],
+                                          json.dumps({"query": {"bool": {"must": body}}, "size": 100}))
+        query_result["hits"] = ranking.reranking(query_result["hits"], args)
+
+        for x in query_result["hits"]:
+            res = {"id": x["_id"], "score": x["_source"]["score"]}
+            for y in x["_source"]:
+                res[y] = x["_source"][y]
+            result.append(res)
+
+    args = dict(request.args)
+    if not ("search_content" in request.args):
+        args["search_content"] = ""
+    if not ("where_to_search" in request.args):
+        args["where_to_search"] = ""
+    if not ("index" in request.args):
+        args["index"] = ""
+    if not ("doc_type" in request.args):
+        args["doc_type"] = ""
+    return render_template("search.html", args=request.args, result=result, query=request.args)
 
 
 @app.route('/adddata', methods=["POST", "GET"])
@@ -181,7 +287,7 @@ def get_doc_byid():
         query_result = elastic.get_by_id(request.args["index"], request.args["doc_type"], request.args["id"])
         return json.dumps(query_result["_source"])
         # print query_result["_source"]["content"]
-        #return render_template("news.html", content=unicode(query_result["_source"]["content"]),
+        # return render_template("news.html", content=unicode(query_result["_source"]["content"]),
         #                       Title=query_result["_source"]["Title"],
         #                       PubDate=query_result["_source"]["PubDate"],
         #                       origin=query_result["_source"]["doc_name"]) \
