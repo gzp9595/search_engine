a05b200cc69e025e8966eada0332793b889f1a8f
zhonghaoxi <zhonghaoxi@yeah.net>
2017-09-08 10:32:35 +0800

    gg

---
+++ tools/init_vector.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("curl -XPUT http://localhost:9200/law_vector")
+print
+
+print
+os.system("""curl -XPOST http://localhost:9200/law_thulac/tfidf/_mapping -d'
+{
+    "tfidf": {
+        "_all": {
+            "type": "text",
+            "index" : "no"
+        }
+    }
+}'""")
