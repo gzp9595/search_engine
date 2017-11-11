b6cb87512b8695cafd7d77350ae0edbec9d90e4a
zhonghaoxi <zhonghaoxi@yeah.net>
2017-11-11 15:41:30 +0800

    gg

--- application/util/util.py
+++ application/util/util.py
@@ -4,6 +4,7 @@ import json
 
 class CJsonEncoder(json.JSONEncoder):
     def default(self, obj):
+        print obj
         if isinstance(obj, datetime.datetime):
             return obj.strftime('%Y-%m-%d %H:%M:%S')
         elif isinstance(obj, date):
