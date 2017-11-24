6f3ca2ac9fd7d03bb8468ff2af25ef23f546793e
zhonghaoxi <zhonghaoxi@yeah.net>
2017-11-24 23:06:36 +0800

    gg

--- submodules/databaser/work.py
+++ submodules/databaser/work.py
@@ -11,6 +11,18 @@ login_manger.init_app(app)
 login_manger.login_view = "login"
 login_manger.login_message = "233"
 
+@app.route('/login/', method=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        pwd = request.form.get("pwd")
+        if name != app.config["NAME"] or pwd != app.config["PWD"]:
+            flash('GG')
+        else:
+            login_user(name, remember=True)
+            next_url = request.args.get('next')
+            return redirect(next_url or url_for('login_success'))
+    return "gg"
 
 @app.route('/')
 @login_required
