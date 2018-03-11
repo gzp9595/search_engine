dd7c6d47a46a87beb8fede75e6d31cd86ab5784e
zhonghaoxi <zhonghaoxi@yeah.net>
2018-03-11 11:34:52 +0800

    thulac, slow

--- application/cutter/cutter.py
+++ application/cutter/cutter.py
@@ -2,11 +2,13 @@ import os
 from application import app
 from application.util import print_time
 import uuid
-import thulac
 
 cutter = None
 
-cutter = thulac.thulac(seg_only=True, model_path=app.config["THULAC_MODEL_PATH"])
+if not ("THULAC" in app.config and os.path.exists(app.config["THULAC"])):
+    import thulac
+
+    cutter = thulac.thulac(seg_only=True)
 
 
 def thulac_cutone(text):
@@ -24,8 +26,33 @@ def cut(text):
         text = [text]
 
     result = []
-    for line in text:
-        result.append(thulac_cutone(line))
+
+    if cutter is None:
+        temp_path = os.path.join(app.config["TEMP_DIR"], str(uuid.uuid4()) + ".txt")
+        temp_path2 = os.path.join(app.config["TEMP_DIR"], str(uuid.uuid4()) + ".txt")
+        f = open(temp_path, "w")
+        for line in text:
+            print(line.replace("\n", "").replace(" ", ""),file=f)
+        f.close()
+
+        os.system(app.config["THULAC"] + "thulac -model_dir " + app.config[
+            "THULAC"] + "models -seg_only -t2s < " + temp_path + " > " + temp_path2)
+        print(app.config["THULAC"] + "thulac -model_dir " + app.config[
+            "THULAC"] + "models -seg_only -t2s < " + temp_path + " > " + temp_path2)
+
+        f = open(temp_path2, "r")
+        cnt = 0
+        for line in f:
+            cnt += 1
+            if cnt == 1:
+                continue
+            data = line.replace("\n", " ").replace("%", " ").split(" ")
+            data = data[0:len(data) - 1]
+            result.append(data)
+    else:
+        result = []
+        for line in text:
+            result.append(thulac_cutone(line))
 
     print("One cut end")
     print_time()
